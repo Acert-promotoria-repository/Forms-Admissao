477acf0b07f61e2fc14f19c9beeee593b836ef31
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2024-10-23 09:36:29 -0300

    Estilos finalizados, media query para mobile, tablets e telas menores adicionadas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-console.log("Hello World")
- 
+/*import emailjs from 'emailjs-com'
+
+emailjs.init("service_forms_admissao")
+
+class Forms{
+    private name:string
+    private attend:string
+    private jobVacancy:string
+    private exp:string
+    private email:string
+    private phoneNumber:string
+    private description:string
+    private form:HTMLFormElement
+    private formInput
+} */
