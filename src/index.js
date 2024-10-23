559b2dd19f2ce8b72290714877f2ca654259651e
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2024-10-23 10:54:36 -0300

    Parte do Backend com ts finalizada

--- src/index.js
+++ src/index.js
@@ -1,2 +1,75 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const emailjs_com_1 = __importDefault(require("emailjs-com"));
+const template_ID = "template_vpi4xsu";
+const ServiceID = "service_forms_admissao";
+const PublicKey = "_eQ20dD_VcIvrwYWO";
+emailjs_com_1.default.init(PublicKey);
+class Forms {
+    constructor() {
+        this.name = document.querySelector("#nome");
+        this.attend = document.querySelector("#curso");
+        this.jobVacancy = document.querySelector("#vaga");
+        this.exp = document.querySelector("#exp");
+        this.email = document.querySelector("#email");
+        this.phoneNumber = document.querySelector("#telefone");
+        this.description = document.querySelector("descricao");
+        this.form = document.querySelector("#form");
+        this.formInput = document.querySelector("#curriculo");
+        this.form.addEventListener("submit", (e) => this.HandleSubmit(e));
+    }
+    validarEmail(email) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        return emailRegex.test(email);
+    }
+    validarTelefone(numero) {
+        const numeroRegex = /^\(\d{2}\)\d{9}$/;
+        return numeroRegex.test(numero);
+    }
+    HandleSubmit(event) {
+        event.preventDefault();
+        if (!this.validarEmail(this.email.value)) {
+            alert("Por favor, insira um e-mail válido do Gmail (@gmail.com).");
+            return;
+        }
+        if (!this.validarTelefone(this.phoneNumber.value)) {
+            alert("Por favor, insira um número de telefone no formato correto, por exemplo (31)9XXXXXXX.");
+        }
+        const formData = new FormData();
+        formData.append("nome", this.name.value);
+        formData.append("curso", this.attend.value);
+        formData.append("vaga", this.jobVacancy.value);
+        formData.append("experiencia", this.exp.value);
+        formData.append("email", this.email.value);
+        formData.append("telefone", this.phoneNumber.value);
+        formData.append("descricao", this.description.value);
+        if (this.formInput.files && this.formInput.files[0]) {
+            formData.append("curriculo", this.formInput.files[0]);
+        }
+        emailjs_com_1.default.sendForm(ServiceID, template_ID, this.form)
+            .then((response) => {
+            alert("Formulário enviado com sucesso");
+            console.log("Sucesso!", response.status, response.text);
+        }), (error) => {
+            alert("Houve um erro");
+            console.log("Falha....", error);
+        };
+        const WhatsAppMessage = `
+            Nome: ${this.name.value}
+            Curso: ${this.attend.value}
+            Vaga: ${this.jobVacancy.value}
+            Experiência: ${this.exp.value}
+            Email: ${this.email.value}
+            Telefone: ${this.phoneNumber.value}
+            Descrição: ${this.description.value}
+        `.replace(/\s+/g, '%20');
+        const WhatsAppURL = `https://wa.me/+5531997661152?text=${WhatsAppMessage}`;
+        window.open(WhatsAppURL, '_blank');
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+    new Forms();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,97 @@
-/*import emailjs from 'emailjs-com'
+import emailjs from 'emailjs-com';
 
-emailjs.init("service_forms_admissao")
+const template_ID = "template_vpi4xsu";
+const ServiceID = "service_forms_admissao";
+const PublicKey = "_eQ20dD_VcIvrwYWO"
 
-class Forms{
-    private name:string
-    private attend:string
-    private jobVacancy:string
-    private exp:string
-    private email:string
-    private phoneNumber:string
-    private description:string
-    private form:HTMLFormElement
-    private formInput
-} */
+emailjs.init(PublicKey)
+
+class Forms {
+    private name: HTMLInputElement;
+    private attend: HTMLInputElement;
+    private jobVacancy: HTMLInputElement;
+    private exp: HTMLInputElement;
+    private email: HTMLInputElement;
+    private phoneNumber: HTMLInputElement;
+    private description: HTMLTextAreaElement;
+    private form: HTMLFormElement;
+    private formInput: HTMLInputElement;
+    
+
+    constructor() {
+        this.name = document.querySelector("#nome") as HTMLInputElement;
+        this.attend = document.querySelector("#curso") as HTMLInputElement;
+        this.jobVacancy = document.querySelector("#vaga") as HTMLInputElement;
+        this.exp = document.querySelector("#exp") as HTMLInputElement;
+        this.email = document.querySelector("#email") as HTMLInputElement;
+        this.phoneNumber = document.querySelector("#telefone") as HTMLInputElement;
+        this.description = document.querySelector("descricao") as HTMLTextAreaElement;
+        this.form = document.querySelector("#form") as HTMLFormElement;
+        this.formInput = document.querySelector("#curriculo") as HTMLInputElement;
+
+        this.form.addEventListener("submit", (e)=> this.HandleSubmit(e))
+    }
+
+    private validarEmail(email:string):boolean{
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/
+        return emailRegex.test(email)
+    }
+
+    private validarTelefone(numero:string):boolean{
+        const numeroRegex =  /^\(\d{2}\)\d{9}$/
+        return numeroRegex.test(numero)
+    }
+
+    private HandleSubmit(event:Event):void{
+        event.preventDefault()
+        
+        if(!this.validarEmail(this.email.value)){
+            alert("Por favor, insira um e-mail válido do Gmail (@gmail.com).")
+            return;
+        } 
+
+        if(!this.validarTelefone(this.phoneNumber.value)){
+            alert("Por favor, insira um número de telefone no formato correto, por exemplo (31)9XXXXXXX.")
+        }
+
+        const formData = new FormData();
+        formData.append("nome", this.name.value);
+        formData.append("curso", this.attend.value);
+        formData.append("vaga", this.jobVacancy.value);
+        formData.append("experiencia", this.exp.value);
+        formData.append("email", this.email.value);
+        formData.append("telefone", this.phoneNumber.value);
+        formData.append("descricao", this.description.value);
+
+        if(this.formInput.files && this.formInput.files[0]){
+            formData.append("curriculo", this.formInput.files[0])
+        }
+
+        emailjs.sendForm(ServiceID, template_ID,this.form)
+        .then((response:any)=>{
+            alert("Formulário enviado com sucesso")
+            console.log("Sucesso!", response.status, response.text)
+        }),(error:any)=>{
+            alert("Houve um erro")
+            console.log("Falha....", error)
+        }
+
+        const WhatsAppMessage = `
+            Nome: ${this.name.value}
+            Curso: ${this.attend.value}
+            Vaga: ${this.jobVacancy.value}
+            Experiência: ${this.exp.value}
+            Email: ${this.email.value}
+            Telefone: ${this.phoneNumber.value}
+            Descrição: ${this.description.value}
+        `.replace(/\s+/g,'%20')
+
+        const WhatsAppURL = `https://wa.me/+5531997661152?text=${WhatsAppMessage}`
+        window.open(WhatsAppURL, '_blank')
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    new Forms()
+})
